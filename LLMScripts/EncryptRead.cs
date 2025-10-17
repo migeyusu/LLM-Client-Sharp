f9e4de3c5cf5de51feff12c1fcf7a0ee89db7dc1
migeyusu <981975491@qq.com>
2025-10-17 18:08:19 +0800

    修复文件

---
+++ LLMScripts/EncryptRead.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace LLMScripts;
+
+/// <summary>
+/// 绕过加密app
+/// </summary>
+public class EncryptRead
+{
+    private static string SafeReadyPs(string filePath)
+    {
+        string command = $"Get-Content -Path '{filePath}'";
+        var psi = new ProcessStartInfo
+        {
+            // Windows PowerShell 5.x
+            FileName = "powershell",
+            Arguments = $"-NoProfile -NonInteractive -Command \"{command}\"",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false, // 必须为 false，才能重定向输出
+            CreateNoWindow = true // 不弹出黑框
+        };
+
+        using var proc = Process.Start(psi)
+                         ?? throw new InvalidOperationException("无法启动 PowerShell 进程。");
+
+        string output = proc.StandardOutput.ReadToEnd();
+        string error = proc.StandardError.ReadToEnd();
+
+        proc.WaitForExit();
+
+        if (proc.ExitCode != 0)
+        {
+            throw new InvalidOperationException(
+                $"PowerShell 退出码 {proc.ExitCode}，错误信息：{error}");
+        }
+
+        return output.Trim();
+    }
+}
--- LLMScripts/Program.cs
+++ LLMScripts/Program.cs
@@ -1,9 +1,91 @@
-﻿namespace LLMScripts;
+﻿using LLMClient.Abstraction;
+using LLMClient.Dialog;
+using LLMClient.Endpoints;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace LLMScripts;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        var collection = new ServiceCollection();
+        var resourceBuilder = ResourceBuilder
+            .CreateDefault()
+            .AddService("TelemetryConsoleQuickstart");
+        // Enable model diagnostics with sensitive data.
+        AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnosticsSensitive", true);
+
+        using var traceProvider = Sdk.CreateTracerProviderBuilder()
+            .SetResourceBuilder(resourceBuilder)
+            .AddSource("Microsoft.SemanticKernel*")
+            .AddConsoleExporter()
+            .Build();
+
+        using var meterProvider = Sdk.CreateMeterProviderBuilder()
+            .SetResourceBuilder(resourceBuilder)
+            .AddMeter("Microsoft.SemanticKernel*")
+            .AddConsoleExporter()
+            .Build();
+        collection.AddLogging(builder =>
+        {
+            builder.AddDebug();
+            // Add OpenTelemetry as a logging provider
+            builder.AddOpenTelemetry(options =>
+            {
+                options.SetResourceBuilder(resourceBuilder);
+                options.AddConsoleExporter();
+                // Format log messages. This is default to false.
+                options.IncludeFormattedMessage = true;
+                options.IncludeScopes = true;
+            });
+            builder.SetMinimumLevel(LogLevel.Information);
+        });
+
+        ILLMChatClient chatClient;
+    }
+}
+
+class Processor
+{
+    public static async Task GetResponse(ILLMChatClient chatClient, string request,
+        SemaphoreSlim clientSemaphore, ILogger? logger = null, int retryCount = 3)
+    {
+        var modelParams = chatClient.Parameters;
+        modelParams.Streaming = false;
+        var dialogContext = new DialogContext([
+            new RequestViewItem() { TextMessage = request, }
+        ]);
+        var response = new CompletedResult();
+        int tryCount = 0;
+        while (tryCount < retryCount)
+        {
+            response = await client.SendRequest(dialogContext, cancellationToken: token);
+            tryCount++;
+            var textResponse = response.FirstTextResponse;
+            if (!string.IsNullOrEmpty(textResponse) && !response.IsInterrupt)
+            {
+                cache.TryAdd(raw, textResponse);
+                return textResponse;
+            }
+        }
+    }
+}
+
+/// <summary>
+/// 用于对TOC执行注释
+/// </summary>
+class TOCANADescriptor
+{
+    private const string root = @"E:\Dev\toc_multisimult_design\00 源码\TOCAnalyzer";
+
+    public async Task Start()
+    {
     }
 }
