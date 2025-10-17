5143861b34d80d6ce69df28078b18bf88021fec7
migeyusu <981975491@qq.com>
2025-10-17 17:39:42 +0800

    adjust files

--- LLMScripts/Program.cs
+++ LLMScripts/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace LLMScripts;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
